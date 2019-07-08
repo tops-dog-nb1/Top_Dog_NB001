1cea04d587de4cf770e2a68dd0cd4c56a59c5158
Tops-Dog <52615893+Tops-Dog@users.noreply.github.com>
2019-07-08 14:38:48 +0800

    Lzz
    李忠忠做的

---
+++ Lzz_Shop_Order_Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZMD_API.Models
+{
+    public class Lzz_Shop_Order_Model
+    {
+        public int a { get; set; }
+    }
+}
